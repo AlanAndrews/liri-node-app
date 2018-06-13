03b8af141199cde2c5a6a3af2b1f956b7c5dd0ca
Alan <andrewsalan88@gmail.com>
2018-06-13 07:07:53 -0500

    twitter almost workin:"

--- liri.js
+++ liri.js
@@ -1,5 +1,29 @@
-require("dotenv").config();
+// require("dotenv").config();
+
+// specifies which argument is the command
+var command = process.argv[2]; 
 
 var keys = require("./keys.js");
+// console.log(keys);
+// var spotify = new Spotify(keys.spotify);
+// var fs = require('fs');
+var twitter = require('twitter');
+var client = new twitter(keys.twitter);
 
-console.log(keys);
+
+// switch-case allows certain blocks of code to be specified based on a condtion/command in node
+switch(command){
+    case "my-tweets":
+    myTweets();
+}
+
+// code block for my-tweets command based on twitter npm documentation
+function myTweets() {
+var params = {screen_name: 'BootcampUt'};
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets.text);
+    }
+  });
+}
