c6e2bb0fb3db3f4e6825b52be129acfc35af0be4
Alan <andrewsalan88@gmail.com>
2018-06-13 18:04:31 -0500

    twitter done. starting spotify

--- liri.js
+++ liri.js
@@ -1,29 +1,50 @@
-// require("dotenv").config();
+require("dotenv").config();
 
 // specifies which argument is the command
 var command = process.argv[2]; 
 
 var keys = require("./keys.js");
 // console.log(keys);
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 // var fs = require('fs');
 var twitter = require('twitter');
 var client = new twitter(keys.twitter);
 
 
-// switch-case allows certain blocks of code to be specified based on a condtion/command in node
+// switch-case allows certain blocks of code to be specified based on a condtion/command in node 
 switch(command){
     case "my-tweets":
     myTweets();
+    break;
+
+    case "spotify-this-song":
+    spotifySong();
 }
+
+function myTweets(){
+    var screenName = {screen_name: 'BootcampUt'};
+    //using the twitter npm documentation. number of tweets can prob be specified in params --> screenName
+    client.get('statuses/user_timeline', screenName, function(error, tweets, response){
+      if(!error){
+        for(var i = 0; i<tweets.length; i++){
+          var date = tweets[i].created_at;
+          console.log("@" + tweets[i].text + " Created At: " + date.substring(0, 19));//makes sure to get 20 tweets
+          console.log("-----------------------");
+        }
+      }else{
+        console.log('Error occurred');
+        console.log(error);
+      }
+    });
+  }
 
 // code block for my-tweets command based on twitter npm documentation
-function myTweets() {
-var params = {screen_name: 'BootcampUt'};
+// function myTweets() {
+// var params = {screen_name: 'BootcampUt'};
 
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      console.log(tweets.text);
-    }
-  });
-}
+// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//     if (!error) {
+//       console.log(tweets[0].text);
+//     }
+//   });
+// }
